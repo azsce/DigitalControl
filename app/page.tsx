4ceabc806ef8ec15a185cd5d7ea1eed0e2b52b8f
Ahmed Hosny <116438978+ahmed3820@users.noreply.github.com>
2024-12-22 14:09:58 +0000

    docusaurus

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import 'katex/dist/katex.min.css';
-import LectureIntroduction from '@/lectures/LectureIntroduction';
-
-const App = () => (
-  <LectureIntroduction />
-);
-
-export default App;
