8893cc2ab3c5a4504b9b2ae679659d9c3b354d72
Ahmed Hosny <116438978+ahmed3820@users.noreply.github.com>
2024-12-22 12:46:41 +0000

    Refactor application structure and add Markdown presentation component

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import ReactMarkdown from 'react-markdown';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
+import React from 'react';
 import 'katex/dist/katex.min.css';
+import LectureIntroduction from '@/lectures/LectureIntroduction';
 
-const App = () => {
-  const [markdown, setMarkdown] = useState('');
-
-  useEffect(() => {
-    fetch('/content.md') // Path to your file in the public directory
-      .then((res) => res.text())
-      .then(setMarkdown);
-  }, []);
-
-  return (
-    <ReactMarkdown
-      children={markdown}
-      remarkPlugins={[remarkMath]}
-      rehypePlugins={[rehypeKatex]}
-    />
-  );
-};
+const App = () => (
+  <LectureIntroduction />
+);
 
 export default App;
--- lectures/LectureIntroduction.tsx
+++ lectures/LectureIntroduction.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import { MDXProvider } from '@mdx-js/react';
-import LectureContent from './LectureContent.mdx';
-import 'katex/dist/katex.min.css';
+"use client";
 
-const LectureIntroduction = () => (
-  <MDXProvider>
-    <LectureContent />
-  </MDXProvider>
-);
+import React, { useEffect, useState } from "react";
+import Markdown from "@/component/Markdown";
+import Presentation from "@/component/Presentation";
+
+const LectureIntroduction = () => {
+  const [markdown, setMarkdown] = useState("");
+
+  useEffect(() => {
+    fetch("/lectures/1.intro.md")
+      .then((res) => res.text())
+      .then(setMarkdown);
+  }, []);
+
+  return (
+    // <Markdown markdown={markdown} />
+    <Presentation markdown={markdown} />
+  );
+};
 
 export default LectureIntroduction;
